d598e30863be0501c50aa475b02814afd9ab4836
Takuya Abe <takuya.abe@asadigital.net>
2022-04-28 17:59:00 +0900

    content max length 500

--- api/models.py
+++ api/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Post(models.Model):
 
     title = models.CharField(max_length=50)
-    content = models.CharField(max_length=3000)
+    content = models.CharField(max_length=500)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
